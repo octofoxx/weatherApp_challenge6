5cdfe6a1bc27a5d473fa1386953136b89cd63674
octofoxx <devilmaycry87x@gmail.com>
2023-06-04 15:20:05 -0700

    fixed dom grabbing vars

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,12 @@ var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="
 
 var forecast5Day = "https://api.openweathermap.org/data/2.5/forecast?q="
 
-var searchArea = document.getElementById("#searchArea")
-var weatherDisplay = document.getElementById("#weatherDisplay")
-var mainDisplay = document.getElementById("#mainDisplay")
-var fiveDayDisplay = document.getElementById("#5dayDisplay")
-var searchHistory = document.getElementById("#searchHistory")
-var citySearch = document.getElementById("#citySearch")
+var searchArea = document.getElementById("searchArea")
+var weatherDisplay = document.getElementById("weatherDisplay")
+var mainDisplay = document.getElementById("mainDisplay")
+var fiveDayDisplay = document.getElementById("5dayDisplay")
+var searchHistory = document.getElementById("searchHistory")
+var citySearch = document.getElementById("citySearch")
 
 //Todo: make call to openweather
 
@@ -27,7 +27,7 @@ var getCityWeather = function(city){
 
 //handle user's input for search
 
-searchArea.addEventListener("submit",submitHandler);
+searchArea.addEventListener("click",submitHandler);
 
 var submitHandler = function(event){
     event.preventDefault();
