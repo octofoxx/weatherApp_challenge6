e4ad1319049d93e6a689ecc60c2cd509e6dbbeed
octofoxx <devilmaycry87x@gmail.com>
2023-06-04 13:11:51 -0700

    added some logic for api call and user input check

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,46 @@
 const APIkey = "54f241fdd09f1d10ee0b6e1696fe5d01"
 
-var currentWeather = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q="
 
-var forecast5Day = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
-
-var cityCode = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
+var forecast5Day = "https://api.openweathermap.org/data/2.5/forecast?q="
 
 var searchArea = document.getElementById("#searchArea")
-
-
 var weatherDisplay = document.getElementById("#weatherDisplay")
-
-
 var mainDisplay = document.getElementById("#mainDisplay")
-
-
 var fiveDayDisplay = document.getElementById("#5dayDisplay")
+var searchHistory = document.getElementById("#searchHistory")
+var citySearch = document.getElementById("#citySearch")
+
+//Todo: make call to openweather
+
+var getCityWeather = function(city){
+  var APIcall=  currentWeather + city + APIkey;
+  fetch(APIcall)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+  });
+}
+
+
+//handle user's input for search
+
+searchArea.addEventListener("submit",submitHandler);
+
+var submitHandler = function(event){
+    event.preventDefault();
+    var cityName = citySearch.value.trim();
+
+    if (cityName){
+        getCityWeather(cityName);
+        citySearch.innerHTML=("");
+    }
+};
+
+//display current weather: icon, date, temp, wind, humidity 
+
+//display 5 day forecast: icon, date, temp, wind, humidity 
+ 
+//save search history and display to page
